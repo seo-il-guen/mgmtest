29a6e7d5ed618ac9bba3b1a3d378cf27056942d0
magma <magma@koreatech.ac.kr>
2021-12-22 14:44:24 +0900

    최초 커밋

---
+++ src/test/java/com/magma/flow/test/FlowTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magma.flow.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
