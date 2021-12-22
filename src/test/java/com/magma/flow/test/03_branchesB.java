41c871fa2730eeee1a32e0204d80e0e20c62776f
magma <magma@koreatech.ac.kr>
2021-12-22 15:10:27 +0900

    HotFix-B

--- src/test/java/com/magma/flow/test/03_branchesB.java
+++ src/test/java/com/magma/flow/test/03_branchesB.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FlowTestApplicationTests3 {
+class FlowTestApplicationTests2 {
 
 	@Test
 	void contextLoads() {
