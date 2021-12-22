033e8a75ab752982a7e272010c2ea2204b061759
magma <magma@koreatech.ac.kr>
2021-12-22 15:35:39 +0900

    Master COMMIT

--- src/test/java/com/magma/flow/test/04_commit.java
+++ src/test/java/com/magma/flow/test/04_commit.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FlowTestApplicationTests4 {
+class FlowTestApplicationTests1 {
 
 	@Test
 	void contextLoads() {
